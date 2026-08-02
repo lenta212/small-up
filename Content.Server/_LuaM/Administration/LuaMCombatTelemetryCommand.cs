6f17c344d55b99403069f3e136e05b2210e10e75
Orvar Od <orvar.od@local>
2026-08-02 10:21:41 +0300

    feat: expand live ship diagnostics

--- Content.Server/_LuaM/Administration/LuaMCombatTelemetryCommand.cs
+++ Content.Server/_LuaM/Administration/LuaMCombatTelemetryCommand.cs
@@ -1,6 +1,8 @@
 using Content.Server.Administration;
 using Content.Server._Mono.NPC.HTN;
 using Content.Shared.Administration;
+using Content.Shared.Projectiles;
+using Content.Shared.Weapons.Ranged.Components;
 using Robust.Shared.Console;
 
 namespace Content.Server._LuaM.Administration;
@@ -23,9 +25,22 @@ public sealed class LuaMCombatTelemetryCommand : IConsoleCommand
         }
 
         var value = _entities.System<ShipTargetingSystem>().GetTelemetry();
+        var activeProjectiles = CountEntities<ProjectileComponent>();
+        var guns = CountEntities<GunComponent>();
+        var npcControllers = CountEntities<ShipTargetingComponent>();
         shell.WriteLine(
             $"ship-targeting updates={value.Updates}, avg={value.AverageUpdateMilliseconds:F3} ms, " +
             $"max={value.MaximumUpdateMilliseconds:F3} ms, controllers={value.LastControllers}, " +
-            $"fire-control-passes={value.LastFireControlPasses}, cannons-inspected={value.LastCannonsInspected}");
+            $"fire-control-passes={value.LastFireControlPasses}, cannons-inspected={value.LastCannonsInspected}; " +
+            $"live projectiles={activeProjectiles}, guns={guns}, ship-targeting-npcs={npcControllers}");
+    }
+
+    private int CountEntities<T>() where T : IComponent
+    {
+        var count = 0;
+        var query = _entities.EntityQueryEnumerator<T>();
+        while (query.MoveNext(out _, out _))
+            count++;
+        return count;
     }
 }
--- Content.Server/_LuaM/Administration/LuaMShipPersistenceCommands.cs
+++ Content.Server/_LuaM/Administration/LuaMShipPersistenceCommands.cs
@@ -9,6 +9,7 @@ using Content.Server._Mono.FireControl;
 using Content.Server.Power.Components;
 using Content.Shared._Crescent.ShipShields;
 using Content.Shared.Administration;
+using Content.Shared.APC;
 using Content.Shared.Power.Components;
 using Robust.Shared.Console;
 
@@ -89,10 +90,16 @@ public sealed class LuaMShipDiagnoseCommand : IConsoleCommand
         var visited = new HashSet<EntityUid>();
         var receivers = 0;
         var unpowered = 0;
+        var apcs = 0;
+        var powerlessApcs = 0;
+        var substations = 0;
+        var powerlessSubstations = 0;
         var nonFinitePower = 0;
         var shields = 0;
+        var brokenShieldLinks = 0;
         var fireControlServers = 0;
         var controllableWeapons = 0;
+        var brokenGunneryLinks = 0;
         pending.Push(ship.Grid);
 
         while (pending.TryPop(out var uid))
@@ -107,6 +114,13 @@ public sealed class LuaMShipDiagnoseCommand : IConsoleCommand
                     unpowered++;
             }
 
+            if (_entities.TryGetComponent(uid, out ApcComponent? apc))
+            {
+                apcs++;
+                if (!apc.MainBreakerEnabled || apc.LastExternalState == ApcExternalPowerState.None)
+                    powerlessApcs++;
+            }
+
             if (_entities.TryGetComponent(uid, out BatteryComponent? battery) && !float.IsFinite(battery.CurrentCharge))
                 nonFinitePower++;
             if (_entities.TryGetComponent(uid, out PowerNetworkBatteryComponent? network))
@@ -116,15 +130,48 @@ public sealed class LuaMShipDiagnoseCommand : IConsoleCommand
                     !float.IsFinite(state.CurrentSupply) || !float.IsFinite(state.CurrentReceiving) ||
                     !float.IsFinite(state.LoadingNetworkDemand))
                     nonFinitePower++;
+
+                var prototype = _entities.GetComponent<MetaDataComponent>(uid).EntityPrototype?.ID;
+                if (prototype?.Contains("Substation", StringComparison.OrdinalIgnoreCase) == true)
+                {
+                    substations++;
+                    if (state.CurrentStorage <= 0f && state.CurrentSupply <= 0f && state.CurrentReceiving <= 0f)
+                        powerlessSubstations++;
+                }
             }
             if (_entities.TryGetComponent(uid, out PowerChargeComponent? charge) && !float.IsFinite(charge.Charge))
                 nonFinitePower++;
-            if (_entities.HasComponent<ShipShieldComponent>(uid))
+            if (_entities.TryGetComponent(uid, out ShipShieldComponent? shield))
+            {
                 shields++;
-            if (_entities.HasComponent<FireControlServerComponent>(uid))
+                if (shield.Source is not { } source || !_entities.EntityExists(source) ||
+                    !_entities.EntityExists(shield.Shielded))
+                    brokenShieldLinks++;
+            }
+            if (_entities.TryGetComponent(uid, out ShipShieldEmitterComponent? emitter) &&
+                (emitter.Shield is not { } emitterShield || !_entities.EntityExists(emitterShield) ||
+                 emitter.Shielded is not { } emitterShielded || !_entities.EntityExists(emitterShielded)))
+                brokenShieldLinks++;
+            if (_entities.TryGetComponent(uid, out FireControlServerComponent? server))
+            {
                 fireControlServers++;
-            if (_entities.HasComponent<FireControllableComponent>(uid))
+                if (server.ConnectedGrid != ship.Grid)
+                    brokenGunneryLinks++;
+                foreach (var controlled in server.Controlled)
+                {
+                    if (!_entities.TryGetComponent(controlled, out FireControllableComponent? weapon) ||
+                        weapon.ControllingServer != uid)
+                        brokenGunneryLinks++;
+                }
+            }
+            if (_entities.TryGetComponent(uid, out FireControllableComponent? controllable))
+            {
                 controllableWeapons++;
+                if (controllable.ControllingServer is not { } controllingServer ||
+                    !_entities.TryGetComponent(controllingServer, out FireControlServerComponent? linkedServer) ||
+                    !linkedServer.Controlled.Contains(uid))
+                    brokenGunneryLinks++;
+            }
 
             var children = _entities.GetComponent<TransformComponent>(uid).ChildEnumerator;
             while (children.MoveNext(out var child))
@@ -134,7 +181,10 @@ public sealed class LuaMShipDiagnoseCommand : IConsoleCommand
         shell.WriteLine(
             $"ship={ship.ShipId:D} grid={ship.Grid} entities={visited.Count}; " +
             $"power-receivers={receivers}, unpowered={unpowered}, non-finite-power-components={nonFinitePower}; " +
-            $"shields={shields}; fire-control-servers={fireControlServers}, controllable-weapons={controllableWeapons}");
+            $"apcs={apcs}, powerless-apcs={powerlessApcs}, substations={substations}, " +
+            $"powerless-substations={powerlessSubstations}; shields={shields}, broken-shield-links={brokenShieldLinks}; " +
+            $"fire-control-servers={fireControlServers}, controllable-weapons={controllableWeapons}, " +
+            $"broken-gunnery-links={brokenGunneryLinks}");
     }
 }
 
